7db563f90ce26dad46e1f45a44818d394ebb8028
Agnislav Onufriichuk <agnislav@gmail.com>
2018-02-15 18:09:07 +0200

    Environment-dependent styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./styles/app.component.scss', './styles/app.component.' + environment.site + '.scss'],
 })
 export class AppComponent {
   title = 'app';
